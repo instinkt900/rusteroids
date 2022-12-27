1a0e1ccc0a8d8cfa510d005d00ee162522a002d6
Matthew Cotton <instinkt900@hotmail.com>
2022-12-27 17:18:43 +1100

    Cleaned up a bunch of constants to help tweaking. Added radius and mass to handle gravity

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,53 @@ use bevy_prototype_debug_lines::*;
 use rand_pcg::Pcg32;
 use rand::{Rng, SeedableRng};
 
+const TIME_STEP: f32 = 1.0 / 60.0;
+
+// drawing constants
 const SHIP_CORNERS: [Vec3; 3] = [
     Vec3 { x:  0.0, y:  5.0, z: 0.0},
     Vec3 { x: -5.0, y: -5.0, z: 0.0},
     Vec3 { x:  5.0, y: -5.0, z: 0.0},
 ];
-
-const TIME_STEP: f32 = 1.0 / 60.0;
-
-const SHIP_ROTATION_SPEED: f32 = 0.05;
-const BULLET_VELOCITY: f32 = 200.0;
-const SHIP_MAX_THRUST: f32 = 1.25;
-
-const GRAVITY_SCALE: f32 = 20000.0;
-
-const SHIP_RADIUS: f32 = 10.0;
-const BULLET_RADIUS: f32 = 1.0;
-
-const PLANET_POINT_COUNT: u32 = 20;
+const PLANET_POINT_COUNT: u32 = 80;
 const ASTEROID_POINT_COUNT: u32 = 15;
 const BULLET_POINT_COUNT: u32 = 4;
+const ASTEROID_RADIUS_VARIANCE: f32 = 2.0;
 
-const ASTEROID_SPAWN_DISTANCE: f32 = 640.0;
+const GRAVITY: f32 = 250.0;
+
+const SHIP_ROTATION_SPEED: f32 = 0.07;
+const SHIP_MAX_THRUST: f32 = 1.25;
+const SHIP_RADIUS: f32 = 10.0;
+const SHIP_MASS: f32 = 10.0;
+const SHIP_FIRE_DELAY: u64 = 100;
+
+const BULLET_VELOCITY: f32 = 300.0;
+const BULLET_RADIUS: f32 = 1.0;
+const BULLET_MASS: f32 = 20.0;
+const BULLET_LIFETIME_MS: u64 = 3000;
+
+const PLANET_START_RADIUS: f32 = 30.0;
+const PLANET_START_MASS: f32 = 500.0;
+const PLANET_RADIUS_CONSUME_SCALE: f32 = 0.1;
+const PLANET_MASS_CONSUME_SCALE: f32 = 1.0;
+
+const ASTEROID_SPAWN_DISTANCE: f32 = 320.0;
+const ASTEROID_LIFETIME_MS: u64 = 30000;
+const ASTEROID_SPAWN_DELAY_MIN_MS: u64 = 2000;
+const ASTEROID_SPAWN_DELAY_MAX_MS: u64 = 4000;
+const ASTEROID_RADIUS_MIN: f32 = 10.0;
+const ASTEROID_RADIUS_MAX: f32 = 20.0;
+const ASTEROID_MASS_MIN: f32 = 10.0;
+const ASTEROID_MASS_MAX: f32 = 20.0;
+const ASTEROID_VELOCITY_MIN: f32 = 10.0;
+const ASTEROID_VELOCITY_MAX: f32 = 40.0;
+const ASTEROID_FRACTURE_COUNT: u32 = 3;             // broken asteroids break into N parts
+const ASTEROID_FRACTURE_RADIUS_FACTOR: f32 = 0.3;   // each broken part has F radius of its parent
+const ASTEROID_FRACTURE_MASS_FACTOR: f32 = 0.2;     // each broken part has F mass of its parent
+const ASTEROID_FRACTURE_MIN_RADIUS: f32 = 4.0;      // any asteroid smaller than this does not fracture
+const ASTEROID_FRACTURE_VEL_MIN: f32 = 10.0;        // min velocity to randomly apply to each fractured part
+const ASTEROID_FRACTURE_VEL_MAX: f32 = 40.0;        // max velocity to randomly apply to each fractured part
 
 #[derive(Resource, Default)]
 struct AsteroidTimer { duration: Duration }
@@ -37,8 +62,8 @@ struct Lifetime(Duration);
 #[derive(Component, Deref, DerefMut)]
 struct Radius(f32);
 
-#[derive(Component)]
-struct Mass { scale: f32 }
+#[derive(Component, Deref, DerefMut)]
+struct Mass(f32);
 
 #[derive(Component)]
 struct Planet;
@@ -60,8 +85,8 @@ fn setup(mut commands: Commands) {
         transform: Transform::from_xyz(0.0, 0.0, 5.0),
         ..Default::default()
     });
-    commands.spawn((Ship { fire_delay: Duration::from_millis(0) }, Radius(SHIP_RADIUS), Mass { scale: 1.0 }, Transform::from_xyz(0.0, 300.0, 0.0), Velocity(Vec2::new(0.0,0.0))));
-    commands.spawn((Planet, Radius( 30.0 ), Transform::from_xyz(0.0, 0.0, 0.0)));
+    commands.spawn((Ship { fire_delay: Duration::from_millis(0) }, Radius(SHIP_RADIUS), Mass(SHIP_MASS), Transform::from_xyz(0.0, 300.0, 0.0), Velocity(Vec2::new(0.0,0.0))));
+    commands.spawn((Planet, Radius(PLANET_START_RADIUS), Mass(PLANET_START_MASS), Transform::from_xyz(0.0, 0.0, 0.0)));
     //commands.spawn((Planet, Radius( 10.0 ), Transform::from_xyz(200.0, -100.0, 0.0)));
     //commands.spawn((Planet, Radius( 10.0 ), Transform::from_xyz(-200.0, -100.0, 0.0)));
     commands.insert_resource(AsteroidTimer{ duration: Duration::from_secs(5) });
@@ -102,13 +127,14 @@ fn ship_control(mut query: Query<(&mut Transform, &mut Velocity), With<Ship>>, k
     }
 }
 
-fn apply_gravity(mut entity_query: Query<(&Mass, &Transform, &mut Velocity)>, planet_query: Query<(&Transform, &Radius), With<Planet>>) {
-    for (planet_transform, planet_radius) in &planet_query {
-        let planet_radius = **planet_radius;
-        for (entity_mass, entity_transform, mut entity_velocity) in &mut entity_query {
+fn apply_gravity(planet_query: Query<(&Transform, &Mass), With<Planet>>, mut entity_query: Query<(&Transform, &Mass, &mut Velocity)>) {
+    for (planet_transform, planet_mass) in &planet_query {
+        let planet_mass = **planet_mass;
+        for (entity_transform, entity_mass, mut entity_velocity) in &mut entity_query {
+            let entity_mass = **entity_mass;
             let gravity_vector = (planet_transform.translation - entity_transform.translation).normalize();
             let distance = (planet_transform.translation - entity_transform.translation).length();
-            let gravity_force = f32::min(planet_radius, distance) * GRAVITY_SCALE * entity_mass.scale / f32::max(1.0, distance * distance);
+            let gravity_force = GRAVITY * (planet_mass * entity_mass) / f32::max(1.0, distance * distance);
             entity_velocity.x += gravity_vector.x * gravity_force * TIME_STEP;
             entity_velocity.y += gravity_vector.y * gravity_force * TIME_STEP;
         }
@@ -153,48 +179,63 @@ fn fire_control(mut query: Query<(&mut Ship, &Transform)>, mut commands: Command
         if ship.fire_delay.is_zero() && keyboard_input.pressed(KeyCode::Space) {
             let bullet_velocity = transform.rotation * Vec3::new(0.0, BULLET_VELOCITY, 0.0);
             let bullet_position = transform.translation + transform.rotation * SHIP_CORNERS[0];
-            commands.spawn((Bullet, Lifetime(Duration::from_secs(3)), Radius(BULLET_RADIUS), Mass { scale: 6.0 }, Transform::from_translation(bullet_position), Velocity ( Vec2::new(bullet_velocity.x, bullet_velocity.y) )));
-            ship.fire_delay = Duration::from_millis(100);
+            commands.spawn((Bullet, Lifetime(Duration::from_millis(BULLET_LIFETIME_MS)), Radius(BULLET_RADIUS), Mass(BULLET_MASS), Transform::from_translation(bullet_position), Velocity ( Vec2::new(bullet_velocity.x, bullet_velocity.y) )));
+            ship.fire_delay = Duration::from_millis(SHIP_FIRE_DELAY);
         }
     }
 }
 
-fn planet_colision(mut commands: Commands, planet_query: Query<(&Radius, &Transform), With<Planet>>, entity_query: Query<(Entity, &Radius, &Transform), Without<Planet>>) {
-    for (planet_radius, planet_transform) in &planet_query {
-        let planet_radius = **planet_radius;
-        for (entity, entity_radius, entity_transform) in &entity_query {
+fn planet_colision(mut commands: Commands, mut planet_query: Query<(&mut Radius, &mut Mass, &Transform), With<Planet>>, entity_query: Query<(Entity, &Radius, &Mass, &Transform, Option<&Bullet>), Without<Planet>>) {
+    for (mut planet_radius, mut planet_mass, planet_transform) in &mut planet_query {
+        let mut planet_radius_value = **planet_radius;
+        let mut planet_mass_value = **planet_mass;
+        for (entity, entity_radius, entity_mass, entity_transform, optional_bullet) in &entity_query {
             let entity_radius = **entity_radius;
+            let entity_mass = **entity_mass;
             let distance = Vec3::distance(planet_transform.translation, entity_transform.translation);
-            if distance < (planet_radius + entity_radius) {
+            if distance < (planet_radius_value + entity_radius) {
                 commands.entity(entity).despawn();
+                if optional_bullet.is_none() {
+                    planet_radius_value += entity_radius * PLANET_RADIUS_CONSUME_SCALE;
+                    planet_mass_value += entity_mass * PLANET_MASS_CONSUME_SCALE;
+                }
             }
         }
+        **planet_radius = planet_radius_value;
+        **planet_mass = planet_mass_value;
     }
 }
 
-fn asteroid_collision(mut commands: Commands, asteroid_query: Query<(Entity, &Radius, &Transform, &Velocity), With<Asteroid>>, bullet_query: Query<(Entity, &Radius, &Transform), With<Bullet>>) {
-    for (asteroid_entity, asteroid_radius, asteroid_transform, asteroid_velocity) in &asteroid_query {
+fn asteroid_collision(mut commands: Commands, asteroid_query: Query<(Entity, &Radius, &Mass, &Transform, &Velocity), With<Asteroid>>, bullet_query: Query<(Entity, &Radius, &Transform), With<Bullet>>) {
+    for (asteroid_entity, asteroid_radius, asteroid_mass, asteroid_transform, asteroid_velocity) in &asteroid_query {
         let asteroid_radius = **asteroid_radius;
+        let asteroid_mass = **asteroid_mass;
         for (bullet_entity, bullet_radius, bullet_transform) in &bullet_query {
             let bullet_radius = **bullet_radius;
             let distance = Vec3::distance(asteroid_transform.translation, bullet_transform.translation);
             if distance < (asteroid_radius + bullet_radius) {
                 commands.entity(asteroid_entity).despawn();
                 commands.entity(bullet_entity).despawn();
-
-                if asteroid_radius > 4.0 {
+                if asteroid_radius > ASTEROID_FRACTURE_MIN_RADIUS {
                     let mut rng = rand::thread_rng();
-                    let new_radius = asteroid_radius / 3.0;
+                    let new_radius = asteroid_radius * ASTEROID_FRACTURE_RADIUS_FACTOR;
+                    let new_mass = asteroid_mass * ASTEROID_FRACTURE_MASS_FACTOR;
                     let max_angle = 2.0 * std::f32::consts::PI;
-                    let angle_section = max_angle / 3.0;
+                    let angle_section = max_angle / (ASTEROID_FRACTURE_COUNT as f32);
                     let mut spawn_angle: f32 = rng.gen_range(0.0..max_angle);
-                    for _i in 0..3 {
+                    for _i in 0..ASTEROID_FRACTURE_COUNT {
                         let spawn_x = asteroid_transform.translation.x + new_radius * spawn_angle.cos();
                         let spawn_y = asteroid_transform.translation.y + new_radius * spawn_angle.sin();
                         let shape_seed = rng.gen::<u64>();
-                        let asteroid_velocity_x = asteroid_velocity.x + spawn_angle.cos() * rng.gen_range(10.0..30.0);
-                        let asteroid_velocity_y = asteroid_velocity.y + spawn_angle.sin() * rng.gen_range(10.0..30.0);
-                        commands.spawn((Asteroid { seed: shape_seed }, Radius( new_radius ), Mass { scale: 1.0 }, Transform::from_xyz(spawn_x, spawn_y, 0.0), Velocity(Vec2::new(asteroid_velocity_x, asteroid_velocity_y))));
+                        let asteroid_velocity_x = asteroid_velocity.x + spawn_angle.cos() * rng.gen_range(ASTEROID_FRACTURE_VEL_MIN..ASTEROID_FRACTURE_VEL_MAX);
+                        let asteroid_velocity_y = asteroid_velocity.y + spawn_angle.sin() * rng.gen_range(ASTEROID_FRACTURE_VEL_MIN..ASTEROID_FRACTURE_VEL_MAX);
+                        commands.spawn((Asteroid { seed: shape_seed },
+                                        Radius(new_radius),
+                                        Mass(new_mass),
+                                        Transform::from_xyz(spawn_x, spawn_y, 0.0), 
+                                        Velocity(Vec2::new(asteroid_velocity_x, asteroid_velocity_y)),
+                                        Lifetime(Duration::from_millis(ASTEROID_LIFETIME_MS))
+                                        ));
                         spawn_angle += angle_section;
                     }
                 }
@@ -214,15 +255,25 @@ fn asteroid_spawner(mut commands: Commands, mut asteroid_timer: ResMut<AsteroidT
 
     if asteroid_timer.duration.is_zero() {
         let mut rng = rand::thread_rng();
-        let spawn_angle: f32 = rng.gen_range(0.0..6.28);
+        let max_angle = 2.0 * std::f32::consts::PI;
+        let spawn_angle: f32 = rng.gen_range(0.0..max_angle);
         let spawn_x = ASTEROID_SPAWN_DISTANCE * spawn_angle.cos();
         let spawn_y = ASTEROID_SPAWN_DISTANCE * spawn_angle.sin();
-        let asteroid_radius = rng.gen_range(7.0..24.0);
+        let asteroid_radius = rng.gen_range(ASTEROID_RADIUS_MIN..ASTEROID_RADIUS_MAX);
+        let asteroid_mass = rng.gen_range(ASTEROID_MASS_MIN..ASTEROID_MASS_MAX);
         let shape_seed = rng.gen::<u64>();
-        let asteroid_velocity_x = rng.gen_range(10.0..50.0);
-        let asteroid_velocity_y = rng.gen_range(10.0..50.0);
-        commands.spawn((Asteroid { seed: shape_seed }, Radius( asteroid_radius ), Mass { scale: 1.0 }, Transform::from_xyz(spawn_x, spawn_y, 0.0), Velocity(Vec2::new(asteroid_velocity_x, asteroid_velocity_y))));
-        asteroid_timer.duration = Duration::from_millis(rng.gen_range(3000..5000));
+        let asteroid_speed = rng.gen_range(ASTEROID_VELOCITY_MIN..ASTEROID_VELOCITY_MAX);
+        let velocity_angle = spawn_angle + max_angle / 3.5;
+        let asteroid_velocity_x = asteroid_speed * velocity_angle.cos();
+        let asteroid_velocity_y = asteroid_speed * velocity_angle.sin();
+        commands.spawn((Asteroid { seed: shape_seed },
+                        Radius(asteroid_radius),
+                        Mass(asteroid_mass),
+                        Transform::from_xyz(spawn_x, spawn_y, 0.0),
+                        Velocity(Vec2::new(asteroid_velocity_x, asteroid_velocity_y)),
+                        Lifetime(Duration::from_millis(ASTEROID_LIFETIME_MS))
+                        ));
+        asteroid_timer.duration = Duration::from_millis(rng.gen_range(ASTEROID_SPAWN_DELAY_MIN_MS..ASTEROID_SPAWN_DELAY_MAX_MS));
     }
 }
 
@@ -286,7 +337,7 @@ fn asteroid_render(query: Query<(&Radius, &Transform, &Asteroid)>, mut lines: Re
     for (radius, transform, asteroid) in &query {
         let position = transform.translation;
         let radius = **radius;
-        draw_irregular_circle(&mut lines, asteroid.seed, position, radius - 2.0, radius + 2.0, Color::GREEN, ASTEROID_POINT_COUNT);
+        draw_irregular_circle(&mut lines, asteroid.seed, position, radius - ASTEROID_RADIUS_VARIANCE, radius + ASTEROID_RADIUS_VARIANCE, Color::GREEN, ASTEROID_POINT_COUNT);
     }
 }
 
